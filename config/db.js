a19fba452f506b604113825ff173d242b6b76638
Mrudula Shasthrulla <mrudula.shasthrulla@collabrix.co>
2025-03-07 20:54:18 +0530

    Git ignore and changed db.js file

--- config/db.js
+++ config/db.js
@@ -1,22 +1,21 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: "postgres",
-        port: process.env.DB_PORT,
-        logging: false,
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true, // Required for Render PostgreSQL
+            rejectUnauthorized: false // Bypasses self-signed SSL issue
+        }
+    },
+    logging: false,
+});
 
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
-        console.log("✅ PostgreSQL Connected...");
+        console.log("✅ PostgreSQL Connected to Render...");
     } catch (error) {
         console.error("❌ PostgreSQL Connection Error:", error.message);
         process.exit(1);
